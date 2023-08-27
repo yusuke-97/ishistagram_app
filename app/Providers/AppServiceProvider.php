d2f0673ae6e8100e59cade272e0528b50cc4c614
Yutan0409 <yankeesyuu@gmail.com>
2023-08-27 22:03:50 +0900

    deploy to heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,8 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \DB::listen(function($query) {
+        \DB::listen(function ($query) {
             \Log::info($query->sql, $query->bindings, $query->time);
-        });        
+        });
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
