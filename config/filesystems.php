5bc9343dd7dfcaba50d0367fe4a12c4c721b9750
Yutan0409 <yankeesyuu@gmail.com>
2023-08-28 16:59:25 +0900

    filesystems.phpの変更

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,8 +34,8 @@ return [
 
     ],
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
-    ],
+    // 'links' => [
+    //     public_path('storage') => storage_path('app/public'),
+    // ],
 
 ];
