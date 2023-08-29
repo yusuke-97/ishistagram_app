08428866cdb27ca6e4e06597fbe9e2fb0a1d9bca
Yutan0409 <yankeesyuu@gmail.com>
2023-08-29 09:13:44 +0900

    post_header_profile.blade.phpの修正2

--- resources/views/components/post_header_profile.blade.php
+++ resources/views/components/post_header_profile.blade.php
@@ -1,7 +1,7 @@
 <!-- 投稿のヘッダープロフィール -->
 <div class="card-header">
     <a href="{{ route('profile.default') }}" style="text-decoration: none; color: inherit;">
-        @if(Auth::user()->profile_image)
+        @if($post->user->profile_image)
         <img class="card-header-profile" src="{{ Storage::disk('s3')->url('profile_images/' . $post->user->profile_image) }}" alt="User's Profile Image">
         @else
         <i class="fas fa-user fa-5x card-header-profile-icon"></i>
