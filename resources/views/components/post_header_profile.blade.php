e0866fef74270b793f016758b472435aa14c2b5f
Yutan0409 <yankeesyuu@gmail.com>
2023-08-29 10:11:22 +0900

    post_header_profileのrouteの条件分岐の追加

--- resources/views/components/post_header_profile.blade.php
+++ resources/views/components/post_header_profile.blade.php
@@ -1,11 +1,21 @@
 <!-- 投稿のヘッダープロフィール -->
 <div class="card-header">
+    @if (Auth::id() === $post->user->id)
+    <a href="{{ route('profile.default') }}" style="text-decoration: none; color: inherit;">
+        @if($post->user->profile_image)
+        <img class="card-header-profile" src="{{ Storage::disk('s3')->url('profile_images/' . $post->user->profile_image) }}" alt="User's Profile Image">
+        @else
+        <i class="fas fa-user fa-5x card-header-profile-icon"></i>
+        @endif
+    </a>
+    @else
     <a href="{{ route('profile.show', ['profile' => $post->user->id]) }}" style="text-decoration: none; color: inherit;">
         @if($post->user->profile_image)
         <img class="card-header-profile" src="{{ Storage::disk('s3')->url('profile_images/' . $post->user->profile_image) }}" alt="User's Profile Image">
         @else
         <i class="fas fa-user fa-5x card-header-profile-icon"></i>
         @endif
-        <span style="font-weight: bold;">{{ $post->user->user_name }}</span>
     </a>
+    @endif
+    <span style="font-weight: bold;">{{ $post->user->user_name }}</span>
 </div>
