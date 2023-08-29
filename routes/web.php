5187e96d0964c83447085cac5398f3840f738466
Yutan0409 <yankeesyuu@gmail.com>
2023-08-29 18:33:33 +0900

    web.php

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,8 @@ Route::get('profile', [ProfileController::class, 'show'])->name('profile.default
 
 
 Route::resource('/likes', LikeController::class)->middleware(['auth', 'verified']);
-Route::post('follow/{userId}', [FollowsController::class, 'follow'])->name('follow');
-Route::delete('unfollow/{userId}', [FollowsController::class, 'unfollow'])->name('unfollow');
+Route::post('follow/{userId}', [FollowsController::class, 'store'])->name('follow');
+Route::delete('unfollow/{userId}', [FollowsController::class, 'destroy'])->name('unfollow');
 
 // Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 Route::resource('/users', UserController::class)->middleware(['auth', 'verified']);
