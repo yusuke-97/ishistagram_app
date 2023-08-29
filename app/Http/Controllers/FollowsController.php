3cb6c45ac33772aebaa077f85f6fb2a291dea2df
Yutan0409 <yankeesyuu@gmail.com>
2023-08-29 15:31:40 +0900

    FollowsController.phpの修正3

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,20 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class FollowsController extends Controller
 {
     public function store(User $user)
     {
-        auth()->user()->following()->attach($user->id);
+        Auth::user()->following()->attach($user->id);
 
         return back();
     }
 
     public function destroy(User $user)
     {
-        auth()->user()->following()->detach($user->id);
+        Auth::user()->following()->detach($user->id);
 
         return back();
     }
