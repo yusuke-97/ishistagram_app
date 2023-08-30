3515f096942ec01b97cbba62201b0279da867dd4
Yutan0409 <yankeesyuu@gmail.com>
2023-08-30 21:02:23 +0900

    Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/profile';
+    protected $redirectTo = '/post/profile';
 
 
     public function logout(Request $request)
