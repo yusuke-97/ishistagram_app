e13fd789bb1556dca3bd0ed60edf0dc740ac37b9
Yutan0409 <yankeesyuu@gmail.com>
2023-08-30 18:04:56 +0900

    web.php

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,4 @@ Route::resource('comments', CommentController::class)->middleware(['auth', 'veri
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
