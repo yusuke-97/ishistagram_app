76ee399d08571be5fe4e13cf99b3adfbefac7de0
Yutan0409 <yankeesyuu@gmail.com>
2023-09-01 16:43:42 +0900

    LikeController.php

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Auth;
 class LikeController extends Controller
 {
     /**
-     * Store a newly created resource in storage.
+     * 与えられた記事に対する「いいね」を作成または削除
+     * すでに「いいね」が存在する場合は削除し、存在しない場合は新たに作成
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -18,12 +19,16 @@ class LikeController extends Controller
     {
         $userId = Auth::id();
         $postId = $request->post_id;
+
+        // 既存の「いいね」を検索
         $existingLike = Like::where('user_id', $userId)->where('post_id', $postId)->first();
 
         if ($existingLike) {
+            // 既存の「いいね」がある場合は削除
             $existingLike->delete();
             return response()->json(['status' => 'unliked']);
         } else {
+            // 「いいね」が存在しない場合は新規作成
             $like = new Like;
             $like->user_id = $userId;
             $like->post_id = $postId;
@@ -33,41 +38,7 @@ class LikeController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Like $like)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Like $like)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Like $like)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
+     * 指定した「いいね」を削除
      *
      * @param  \App\Models\Like  $like
      * @return \Illuminate\Http\Response
