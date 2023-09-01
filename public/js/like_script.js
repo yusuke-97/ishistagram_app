63c5540ed0608d91499e70652b630b3dbb4373e7
Yutan0409 <yankeesyuu@gmail.com>
2023-09-01 13:12:59 +0900

    like_script.js

--- public/js/like_script.js
+++ public/js/like_script.js
@@ -1,9 +1,17 @@
 function onLikeButtonClicked(postId) {
+    // いいねを行うためのフォームを取得
     var form = document.getElementById('like-form-' + postId);
+    
+    // フォームのアクション(URL)を取得
     var action = form.action;
+
+    // CSRFトークンを取得 (Laravelのセキュリティ対策のため)
     var csrfToken = form.querySelector('input[name="_token"]').value;
+    
+    // ポストIDを取得
     var postInput = form.querySelector('input[name="post_id"]').value;
 
+    // フェッチAPIを使用して非同期のHTTPリクエストを実行
     fetch(action, {
         method: 'POST',
         headers: {
@@ -14,11 +22,13 @@ function onLikeButtonClicked(postId) {
             post_id: postInput
         })
     })
-    .then(response => response.json())
+    .then(response => response.json()) // レスポンスをJSON形式で解析
     .then(data => {
+        // いいねのアイコンとカウントの要素を取得
         var likeIcon = document.getElementById('like-icon-' + postId);
         var likeCount = document.getElementById('like-count-' + postId);
         
+        // サーバーからのレスポンスに応じて、アイコンとカウントを更新
         if (data.status === 'liked') {
             likeIcon.style.color = 'red';
             likeIcon.classList.remove('far');
