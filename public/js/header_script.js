692eb5db0c75ddb3acade1d19357bf5c151d71ac
Yutan0409 <yankeesyuu@gmail.com>
2023-09-01 13:00:51 +0900

    header_script.js

--- public/js/header_script.js
+++ public/js/header_script.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Navbarの要素を取得
     var navbar = document.querySelector('#navbarSupportedContent'); // Navbarの要素を取得
+    
+    // プロフィールドロップダウンの要素を取得
     var profileDropdown = document.querySelector('.header-dropdown'); 
+    
+    // ナビバートグルの要素を取得
     var navbarToggler = document.querySelector('.navbar-toggler');
     
+    // BootstrapのCollapse機能を利用するためのインスタンス生成
     var myCollapse = new bootstrap.Collapse(navbar, { toggle: false }); // BootstrapのCollapse機能を利用するためのインスタンス生成
 
+    // プロフィールドロップダウンが存在する場合のロジック
     if (profileDropdown) {
         var profileDropdownTrigger = profileDropdown.querySelector('#navbarDropdown');
         var profileDropdownMenu = profileDropdown.querySelector('.dropdown-menu');
 
+        // プロフィールドロップダウンのインスタンスを作成
         var myDropdown = new bootstrap.Dropdown(profileDropdownTrigger);
 
         // 全体のクリックイベント
         document.addEventListener('click', function (event) {
-            // ナビゲーションを閉じる条件
+            // クリックがNavbarかToggler以外の場合にNavbarを閉じる
             if (navbar && !navbar.contains(event.target) && !event.target.classList.contains('navbar-toggler-icon')) {
                 myCollapse.hide();
             }
 
-            // ドロップダウンメニューを閉じる条件
+            // クリックがプロフィールドロップダウントリガーやメニュー以外の場合にドロップダウンを閉じる
             if (!profileDropdownTrigger.contains(event.target) && !profileDropdownMenu.contains(event.target)) {
                 myDropdown.hide();
             }
@@ -29,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
             event.preventDefault();
             event.stopPropagation();
             
+            // ドロップダウンメニューの表示/非表示を切り替える
             if (profileDropdownMenu.classList.contains('show')) {
                 myDropdown.hide();
             } else {
@@ -42,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
         navbarToggler.addEventListener('click', function(event) {
             event.stopPropagation();
             
+            // Navbarの表示/非表示を切り替える
             if (navbar.classList.contains('show')) {
                 myCollapse.hide();
             } else {
@@ -55,10 +65,14 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }    
 
+    // メインコンテンツの高さを調整
     adjustMainHeight();
+
+    // ウィンドウサイズが変更された場合、メインコンテンツの高さを再調整
     window.addEventListener('resize', adjustMainHeight);
 });
 
+// ヘッダーの高さに合わせて、メインコンテンツの上部マージンを調整する関数
 function adjustMainHeight() {
     var headerHeight = document.querySelector('.navbar.fixed-top').offsetHeight;
     var mainElement = document.querySelector('main');
