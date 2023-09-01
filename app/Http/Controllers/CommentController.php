e70b69a79a99291d59f997ad2c85fb709c6d25c1
Yutan0409 <yankeesyuu@gmail.com>
2023-09-01 16:28:36 +0900

    CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -9,55 +9,39 @@ use Illuminate\Http\Request;
 class CommentController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * リクエストから送られた情報をもとに新しいコメントをデータベースに保存
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        // リクエストのバリデーション
         $request->validate([
             'content' => 'required|string|max:255',
             'post_id' => 'required|integer|exists:posts,id',
         ]);
 
+        // 新しいコメントのインスタンスを作成
         $comment = new Comment();
         $comment->user_id = auth()->id();
         $comment->post_id = $request->post_id;
         $comment->content = $request->content;
         $comment->save();
 
+        // 現在のユーザー名を取得
         $userName = auth()->user()->user_name;
 
+        // 成功した場合、JSONレスポンスを返す
         return response()->json([
             'success' => 'コメントを投稿しました！',
             'user_name' => $userName,
             'id' => $comment->id
-        ]);        
+        ]);
     }
 
     /**
-     * Display the specified resource.
+     * 指定されたコメントを表示
      *
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
@@ -68,30 +52,7 @@ class CommentController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
+     * 指定されたコメントをデータベースから削除
      *
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
@@ -99,7 +60,7 @@ class CommentController extends Controller
     public function destroy(Comment $comment)
     {
         // コメントのオーナーのみがコメントを削除できるようにする
-        if(auth()->id() != $comment->user_id) {
+        if (auth()->id() != $comment->user_id) {
             return response()->json(['error' => '許可されていない操作です。'], 403);
         }
 
