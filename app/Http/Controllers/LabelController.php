2a1eb099b054319376941de8999f81e9bf84765d
Yutan0409 <yankeesyuu@gmail.com>
2023-09-01 16:40:20 +0900

    LabelController.php

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 class LabelController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * ラベルのリストを表示
      *
      * @return \Illuminate\Http\Response
      */
@@ -26,37 +26,29 @@ class LabelController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * 新しいラベルを保存
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $labels = $request->json()->get('labels'); // 送信されたJSONデータからラベルを取得
+        // リクエストからラベルの情報を取得
+        $labels = $request->json()->get('labels');
 
-        // バリデーション。この部分はラベルの形式に応じて調整が必要かもしれません
+        // バリデーションルールの適用
         $request->validate([
             'labels' => 'required|array',
         ]);
 
-        $userId = Auth::id(); // ログインしているユーザーのIDを取得
+        // 現在のユーザーIDを取得
+        $userId = Auth::id();
 
         foreach ($labels as $labelName) {
-            // ユーザーIDとラベル名で既存のラベルを検索
+            // 同じユーザーの既存のラベルを検索
             $existingLabel = Label::where('name', $labelName)->where('user_id', $userId)->first();
 
-            // 既存のラベルが存在しない場合のみ新規作成
+            // ラベルがまだ存在しない場合に新しいラベルを作成
             if (!$existingLabel) {
                 $label = new Label;
                 $label->name = $labelName;
@@ -65,52 +57,7 @@ class LabelController extends Controller
             }
         }
 
-        // 成功時のレスポンス
+        // 成功時のレスポンスを返す
         return response()->json(['message' => 'ラベルを作成しました']);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Label  $label
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Label $label)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Label  $label
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Label $label)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Label  $label
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Label $label)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Label  $label
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Label $label)
-    {
-        //
     }
 }
